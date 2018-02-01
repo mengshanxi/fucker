106d08484f576e63a187546da343b655482322ba
mengshanxi <13811668973@163.com>
2018-02-01 10:01:06 +0800

    init

---
+++ src/main/java/com/self/fucker/FuckerApplication.java
@@ -0,0 +1,12 @@
+package com.self.fucker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/self/fucker/FuckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.self.fucker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FuckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
